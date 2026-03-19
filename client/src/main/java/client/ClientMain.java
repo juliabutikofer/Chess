93d33e9db0d556c0dd8d0839746425d887813dac
Julia <jbutiko2@byu.edu>
2026-03-18 19:27:04 -0600

    clientmain connection

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,10 +1,26 @@
+//package client;
+//
+//import chess.*;
+//
+//public class ClientMain {
+//    public static void main(String[] args) {
+//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+//        System.out.println("♕ 240 Chess Client: " + piece);
+//    }
+//}
 package client;
 
-import chess.*;
+import ui.PreloginUI;
 
 public class ClientMain {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+
+        int port = 8080; // your server port
+
+        ServerFacade facade = new ServerFacade(port);
+
+        PreloginUI prelogin = new PreloginUI(facade);
+
+        prelogin.start();
     }
 }
